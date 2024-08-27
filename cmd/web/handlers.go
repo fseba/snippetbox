405baf8e2b2f4cd402be7aa1e3c4343b1c82c0ac
Sebastian Frueh <sfrueh@gmx.net>
2024-08-27 21:59:09 +0200

    Add about page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -15,6 +15,11 @@ func ping(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func (app *application) about(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+	app.render(w, http.StatusOK, "about.tmpl", data)
+}
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	snippets, err := app.snippets.Latest()
 	if err != nil {
